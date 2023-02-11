b084c24b3761c610bbcd2ed4da111434de942e99
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-02-11 07:34:18 +0000

    cleanup

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,20 @@
+//@ts-check
+
+import { withNx } from '@nrwl/next/plugins/with-nx.js';
+
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    svgr: false,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+};
+
+export default withNx(nextConfig);
